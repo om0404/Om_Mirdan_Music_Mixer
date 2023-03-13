0f7b69682033a0df91623b960a9971b1a00b032e
mirdan22 <113371529+mirdan22@users.noreply.github.com>
2023-03-13 13:59:21 -0400

    icons and board
    added

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-console.log('JavaScript is running!');
+let theButtons = document.querySelectorAll("#buttonHolder img"), 
+	theHeading = document.querySelector("#headLine h1"),
+
+	iconsBoard = document.querySelector(".drop-board"),
+	musicPieces = document.querySelectorAll(".dj-icons img"),
+	dropZones = document.querySelectorAll(".drop-zone"),
+    piecesBoard = document.querySelector(".dj-icons"),	
+
+    draggedPiece; 
