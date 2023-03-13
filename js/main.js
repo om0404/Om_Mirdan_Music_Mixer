f9d28649cb975ff51d4b5cfc1dbf067b6618768e
mirdan22 <113371529+mirdan22@users.noreply.github.com>
2023-03-13 14:01:42 -0400

    js drag function
    added

--- js/main.js
+++ js/main.js
@@ -8,7 +8,54 @@ let theButtons = document.querySelectorAll("#buttonHolder img"),
 
     draggedPiece; 
 
+    function changeBGImage() {
 
+
+ 
+        dropZones.forEach(zone => {
+            while (zone.firstChild) {
+                zone.removeChild(zone.firstChild);
+            }
+         });
+    
+            musicPieces.forEach(piece => {
+            piece.classList.remove('dropped');
+            piecesBoard.appendChild(piece);
+            });
+    
+    
+    
+        iconsBoard.style.backgroundImage = `url('images/backGround${this.id}.jpg')`;
+    }
+    
+    function handleStartDrag() {
+        console.log('started dragging this piece:', this);
+      
+    
+        draggedPiece = this;
+        
+    }
+    
+    function handleDragOver(e) { 
+      
+        e.preventDefault();
+    
+    
+        console.log('dragged over me');
+    }
+    
+    function handleDrop(e) {
+        e.preventDefault();
+        console.log('dropped something on me');
+    
+    
+    
+    
+        if (this.children.length > 0) {
+            return;
+        }
+        this.appendChild(draggedPiece);}
+    
 
 
     theButtons.forEach(button => button.addEventListener("click", changeBGImage));
